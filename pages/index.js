74ca21c42537ba20580ab489ede241b408e635a0
Hakuto09 <bis2023a@gmail.com>
2025-04-04 15:45:31 +0900

    20250404r01-004_udemy-lecture

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,30 @@ import Image from "next/image";
 import Head from "next/head";
 import Link from "next/link";
 import Layout from "@/components/Layout";
-
-
+import utilStype from "../styles/utils.module.css"
 export default function Home() {
   return <Layout>
-    hakuto test
+    <a href="https://www.hakuto.co.jp/">Hakuto site (test)</a>
+    <section className={utilStyle.headingMd}>
+      <p>
+        フルスタックエンジニアです。
+      </p>
+    </section>
+
+    <section>
+      <h2>📝エンジニアのブログ</h2>
+      <div>
+        <article>
+          <Link href="/">
+            <img src="images/thumbnail01.jpg" alt="" />
+          </Link>
+          <Link href="/">
+            <a>SSGとSSRの使い分けの場面はいつなのか？</a>
+          </Link>
+          <br />
+          <small>February 23, 2020</small>
+        </article>
+      </div>
+    </section>
   </Layout>
 }
